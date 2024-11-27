e12545d527eec6585de68ada11e5b635820a2b76
Yugavart Tyagi <tyagiyugavart@gmail.com>
2024-11-27 16:14:03 +0530

    Initial commit for new repository

---
+++ src/main/java/com/example/scm_deploy/ScmDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.scm_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scm_deploy/ScmDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scm_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
